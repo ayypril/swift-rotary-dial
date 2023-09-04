8d2d61cb84249cbe47c9743e808d88a6874df621
April <84289343+ayypril@users.noreply.github.com>
2023-09-04 10:31:20 -0400

    Initial Commit

---
+++ rotary/rotaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  rotaryApp.swift
+//  rotary
+//
+//  Created by April Trainor on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct rotaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
